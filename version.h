87bd1c0c3e7c588218845da87b97823780968cac
Thomas E. Dickey <dickey@invisible-island.net>
2000-08-23 22:30:19 -0400

    snapshot of project "xterm", label xterm-144

--- version.h
+++ version.h
@@ -6,5 +6,5 @@
  * XFree86 to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   143
+#define XTERM_PATCH   144
 #define XFREE86_VERSION "XFree86 4.0.1b"
