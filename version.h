b1b00ca86b4294df555b89ac1deb7bb7c14b4deb
Thomas E. Dickey <dickey@invisible-island.net>
2001-04-21 12:31:31 -0400

    snapshot of project "xterm", label xterm-155

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/xterm/version.h,v 3.66 2001/04/12 01:02:51 dickey Exp $ */
+/* $XFree86: xc/programs/xterm/version.h,v 3.67 2001/04/20 09:30:39 dickey Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -6,5 +6,5 @@
  * XFree86 to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
-#define XTERM_PATCH   154
+#define XTERM_PATCH   155
 #define XFREE86_VERSION "XFree86 4.0.3"
