114ca1344f9a856f029e1e081daefb618688b71a
Thomas E. Dickey <dickey@invisible-island.net>
2008-02-28 01:11:28 -0500

    snapshot of project "xterm", label xterm-233a

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-/* $XTermId: version.h,v 1.289 2008/02/20 20:32:21 tom Exp $ */
+/* $XTermId: version.h,v 1.290 2008/02/28 00:17:03 tom Exp $ */
 
 /*
  * These definitions are used to build the string that's printed in response to
@@ -6,7 +6,7 @@
  * version of X to which this version of xterm has been built.  The number in
  * parentheses is my patch number (Thomas E. Dickey).
  */
-#define XTERM_PATCH   233
+#define XTERM_PATCH   234
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
