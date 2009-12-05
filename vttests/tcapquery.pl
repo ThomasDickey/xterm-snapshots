ca278d62b3a126c9159e16f511fe56b392b8ac0e
Thomas E. Dickey <dickey@invisible-island.net>
2009-12-05 16:04:21 -0500

    snapshot of project "xterm", label xterm-251j

--- vttests/tcapquery.pl
+++ vttests/tcapquery.pl
@@ -1,7 +1,36 @@
 #!/usr/bin/perl -w
-# $XTermId: tcapquery.pl,v 1.13 2008/10/05 16:20:14 tom Exp $
+# $XTermId: tcapquery.pl,v 1.14 2008/10/05 16:20:14 tom Exp $
+# -----------------------------------------------------------------------------
+# this file is part of xterm
 #
-# -- Thomas Dickey (2004/3/3)
+# Copyright 2004-2007,2008 by Thomas E. Dickey
+# 
+#                         All Rights Reserved
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# Except as contained in this notice, the name(s) of the above copyright
+# holders shall not be used in advertising or otherwise to promote the
+# sale, use or other dealings in this Software without prior written
+# authorization.
+# -----------------------------------------------------------------------------
 # Test the tcap-query option of xterm.
 
 use strict;
