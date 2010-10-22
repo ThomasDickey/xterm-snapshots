18552558d39754cb6e69db21742e04d5d1575f45
Thomas E. Dickey <dickey@invisible-island.net>
2010-10-22 00:58:11 -0400

    snapshot of project "xterm", label xterm-264a

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 /*
- * $XTermId: version.h,v 1.322 2010/10/14 08:33:49 tom Exp $
+ * $XTermId: version.h,v 1.323 2010/10/22 00:54:53 tom Exp $
  * ----------------------------------------------------------------------------
  * this file is part of xterm
  *
@@ -39,7 +39,7 @@
  * version of X to which this version of xterm has been built.  The number in
  * parentheses is my patch number (Thomas E. Dickey).
  */
-#define XTERM_PATCH   264
+#define XTERM_PATCH   265
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
