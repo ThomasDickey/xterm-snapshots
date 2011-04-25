accca8d4181491e359f25ae28c7c0842c0f313bc
Thomas E. Dickey <dickey@invisible-island.net>
2011-04-24 22:58:56 -0400

    snapshot of project "xterm", label xterm-269i

--- menu.h
+++ menu.h
@@ -1,4 +1,4 @@
-/* $XTermId: menu.h,v 1.123 2011/04/20 08:51:43 tom Exp $ */
+/* $XTermId: menu.h,v 1.124 2011/04/23 12:14:21 tom Exp $ */
 
 /*
  * Copyright 1999-2010,2011 by Thomas E. Dickey
@@ -139,6 +139,7 @@ extern void HandleTekReset         PROTO_XT_ACTIONS_ARGS;
 extern void HandleTiteInhibit      PROTO_XT_ACTIONS_ARGS;
 extern void HandleToolbar          PROTO_XT_ACTIONS_ARGS;
 extern void HandleUTF8Mode         PROTO_XT_ACTIONS_ARGS;
+extern void HandleUTF8Fonts        PROTO_XT_ACTIONS_ARGS;
 extern void HandleUTF8Title        PROTO_XT_ACTIONS_ARGS;
 extern void HandleVisibility       PROTO_XT_ACTIONS_ARGS;
 
@@ -288,8 +289,9 @@ typedef enum {
     fontMenu_render_font,
 #endif
 #if OPT_WIDE_CHARS
-    fontMenu_wide_chars,
-    fontMenu_wide_title,
+    fontMenu_utf8_mode,
+    fontMenu_utf8_fonts,
+    fontMenu_utf8_title,
 #endif
 #endif
 #if OPT_ALLOW_XXX_OPS
@@ -472,9 +474,11 @@ extern void update_font_renderfont(void);
 
 #if OPT_WIDE_CHARS
 extern void update_font_utf8_mode(void);
+extern void update_font_utf8_fonts(void);
 extern void update_font_utf8_title(void);
 #else
 #define update_font_utf8_mode() /* nothing */
+#define update_font_utf8_fonts() /* nothing */
 #define update_font_utf8_title() /* nothing */
 #endif
 
