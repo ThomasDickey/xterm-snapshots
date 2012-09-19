15afb124476243f16e0fba48658d7825e4bdc7f5
Thomas E. Dickey <dickey@invisible-island.net>
2012-09-19 10:43:55 -0400

    snapshot of project "xterm", label xterm-281j

--- vttests/256colors2.pl
+++ vttests/256colors2.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
-# $XTermId: 256colors2.pl,v 1.10 2009/10/10 14:45:26 tom Exp $
+# $XTermId: 256colors2.pl,v 1.12 2012/09/19 00:31:42 tom Exp $
 # -----------------------------------------------------------------------------
 # this file is part of xterm
 #
-# Copyright 1999-2007,2009 by Thomas E. Dickey
+# Copyright 1999-2009,2012 by Thomas E. Dickey
 # Copyright 2002 by Steve Wall
 # Copyright 1999 by Todd Larason
 # 
@@ -42,10 +42,12 @@ use strict;
 
 use Getopt::Std;
 
-our ($opt_h, $opt_q, $opt_r);
-&getopts('hqr') || die("Usage: $0 [-q] [-r]");
+our ($opt_8, $opt_d, $opt_h, $opt_q, $opt_r);
+&getopts('8dhqr') || die("Usage: $0 [-q] [-r]");
 die("Usage: $0 [options]\n
 Options:
+  -8  use 8-bit controls
+  -d  use rgb values rather than palette index
   -h  display this message
   -q  quieter output by merging all palette initialization
   -r  display the reverse of the usual palette
@@ -53,6 +55,9 @@ Options:
 
 our ($red, $green, $blue);
 our ($gray, $level, $color);
+our ($csi, $osc, $st);
+
+our @rgb;
 
 sub map_cube($) {
 	my $value = $_[0];
@@ -66,18 +71,48 @@ sub map_gray($) {
 	return $value;
 }
 
-printf("\x1b]4") if ($opt_q);
+sub define_color($$$$) {
+	my $index = $_[0];
+	my $r = $_[1];
+	my $g = $_[2];
+	my $b = $_[3];
+
+	printf("%s4", $osc) unless ($opt_q);
+	printf(";%d;rgb:%2.2x/%2.2x/%2.2x", $index, $r, $g, $b);
+	printf("%s", $st) unless ($opt_q);
+
+	$rgb[$index] = sprintf "%d:%d:%d", $r, $g, $b;
+}
+
+sub select_color($) {
+	my $index = $_[0];
+	if ( $opt_d and defined($rgb[$index]) ) {
+		printf "%s48;2:%sm  ", $csi, $rgb[$index];
+	} else {
+		printf "%s48;5;%sm  ", $csi, $index;
+	}
+}
+
+if ( $opt_8 ) {
+	$csi = "\x9b";
+	$osc = "\x9d";
+	$st = "\x9c";
+} else {
+	$csi = "\x1b[";
+	$osc = "\x1b]";
+	$st = "\x1b\\";
+}
+
+printf("%s4", $osc) if ($opt_q);
 # colors 16-231 are a 6x6x6 color cube
 for ($red = 0; $red < 6; $red++) {
     for ($green = 0; $green < 6; $green++) {
 	for ($blue = 0; $blue < 6; $blue++) {
-	    printf("\x1b]4") unless ($opt_q);
-	    printf(";%d;rgb:%2.2x/%2.2x/%2.2x",
+	    &define_color(
 		   16 + (map_cube($red) * 36) + (map_cube($green) * 6) + map_cube($blue),
 		   ($red ? ($red * 40 + 55) : 0),
 		   ($green ? ($green * 40 + 55) : 0),
 		   ($blue ? ($blue * 40 + 55) : 0));
-	    printf("\x1b\\") unless ($opt_q);
 	}
     }
 }
@@ -86,12 +121,9 @@ for ($red = 0; $red < 6; $red++) {
 # black and white
 for ($gray = 0; $gray < 24; $gray++) {
     $level = (map_gray($gray) * 10) + 8;
-    printf("\x1b]4") unless ($opt_q);
-    printf(";%d;rgb:%2.2x/%2.2x/%2.2x",
-	   232 + $gray, $level, $level, $level);
-    printf("\x1b\\") unless ($opt_q);
+    &define_color(232 + $gray, $level, $level, $level);
 }
-printf("\x1b\\") if ($opt_q);
+printf("%s", $st) if ($opt_q);
 
 
 # display the colors
@@ -99,13 +131,13 @@ printf("\x1b\\") if ($opt_q);
 # first the system ones:
 print "System colors:\n";
 for ($color = 0; $color < 8; $color++) {
-    print "\x1b[48;5;${color}m  ";
+    &select_color($color);
 }
-print "\x1b[0m\n";
+printf "%s0m\n", $csi;
 for ($color = 8; $color < 16; $color++) {
-    print "\x1b[48;5;${color}m  ";
+    &select_color($color);
 }
-print "\x1b[0m\n\n";
+printf "%s0m\n\n", $csi;
 
 # now the color cube
 print "Color cube, 6x6x6:\n";
@@ -113,9 +145,9 @@ for ($green = 0; $green < 6; $green++) {
     for ($red = 0; $red < 6; $red++) {
 	for ($blue = 0; $blue < 6; $blue++) {
 	    $color = 16 + ($red * 36) + ($green * 6) + $blue;
-	    print "\x1b[48;5;${color}m  ";
+	    &select_color($color);
 	}
-	print "\x1b[0m ";
+	printf "%s0m ", $csi;
     }
     print "\n";
 }
@@ -124,6 +156,6 @@ for ($green = 0; $green < 6; $green++) {
 # now the grayscale ramp
 print "Grayscale ramp:\n";
 for ($color = 232; $color < 256; $color++) {
-    print "\x1b[48;5;${color}m  ";
+    &select_color($color);
 }
-print "\x1b[0m\n";
+printf "%s0m\n", $csi;
