94136a009fbc3512e743f29c6b865a98eb6cad11
Thomas E. Dickey <dickey@invisible-island.net>
2017-06-01 08:21:10 +0000

    snapshot of project "xterm", label xterm-328

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
-/* $XTermId: version.h,v 1.432 2016/10/24 22:07:15 tom Exp $ */
+/* $XTermId: version.h,v 1.433 2017/06/01 08:20:40 tom Exp $ */
 
 /*
- * Copyright 1998-2015,2016 by Thomas E. Dickey
+ * Copyright 1998-2016,2017 by Thomas E. Dickey
  *
  *                         All Rights Reserved
  *
@@ -39,7 +39,7 @@
  * number in parentheses is my patch number (Thomas E. Dickey).
  */
 #define XTERM_PATCH   328
-#define XTERM_DATE    2016-10-24
+#define XTERM_DATE    2017-06-01
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
