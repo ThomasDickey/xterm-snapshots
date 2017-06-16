99c247230146d70fabbd5f625a943d46f7716970
Thomas E. Dickey <dickey@invisible-island.net>
2017-06-16 23:52:46 +0000

    snapshot of project "xterm", label xterm-329d

--- wcwidth.h
+++ wcwidth.h
@@ -1,9 +1,13 @@
+/* $XTermId: wcwidth.h,v 1.13 2017/06/16 23:29:40 tom Exp $ */
+
 /* $XFree86: xc/programs/xterm/wcwidth.h,v 1.5 2005/05/03 00:38:25 dickey Exp $ */
 
 #ifndef	included_wcwidth_h
 #define	included_wcwidth_h 1
 
 #include <stddef.h>
+
+extern void mk_wcwidth_init(int mode);
 
 extern int mk_wcswidth(const wchar_t * pwcs, size_t n);
 extern int mk_wcswidth_cjk(const wchar_t * pwcs, size_t n);
