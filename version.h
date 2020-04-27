33de6b3d45d23d35b77d4868d9d910c48f10ad41
Thomas E. Dickey <dickey@invisible-island.net>
2020-04-27 00:15:45 +0000

    snapshot of project "xterm", label xterm-354

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
-/* $XTermId: version.h,v 1.487 2020/02/29 00:23:02 tom Exp $ */
+/* $XTermId: version.h,v 1.488 2020/04/27 00:14:01 tom Exp $ */
 
 /*
  * Copyright 1998-2019,2020 by Thomas E. Dickey
@@ -39,7 +39,7 @@
  * number in parentheses is my patch number (Thomas E. Dickey).
  */
 #define XTERM_PATCH   354
-#define XTERM_DATE    2020-02-28
+#define XTERM_DATE    2020-04-26
 
 #ifndef __vendorversion__
 #define __vendorversion__ "XTerm"
